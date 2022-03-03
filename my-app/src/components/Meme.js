dd5ec976e9cb2b5501d0737bb13513c392a0cd95
louie camacho dxa <louiecamacho@gmail.com>
2022-03-03 15:40:44 -0600

    text inputs working

--- my-app/src/components/Meme.js
+++ my-app/src/components/Meme.js
@@ -2,24 +2,60 @@ import React from 'react';
 import memesData from '../memesData';
 
 function Meme() {
-	const [memeImg, setMemeImg] = React.useState('');
+	// const [memeImg, setMemeImg] = React.useState('http://i.imgflip.com/1bij.jpg');
+	const [formData, setFormData] = React.useState({
+		textOne: '',
+		textTwo: '',
+		randomImage: 'http://i.imgflip.com/1bij.jpg',
+	});
+	console.log(formData);
+	function handleChange(event) {
+		const { name, value } = event.target;
+		setFormData((prevText) => ({
+			...prevText,
+			[name]: value,
+		}));
+	}
+
+	const [allMemesImg, setAllMemesImg] = React.useState(memesData);
 
 	function randImg() {
-		const memeArray = memesData.data.memes;
+		const memeArray = allMemesImg.data.memes;
 		const randNum = Math.floor(Math.random() * memeArray.length);
 		const imgUrl = memeArray[randNum].url;
-		setMemeImg(imgUrl);
+		setFormData((prevMeme) => ({
+			...prevMeme,
+			randomImage: imgUrl,
+		}));
 	}
 
 	return (
 		<main>
 			<div className="form">
-				<input className="form--input" type="text" placeholder="text top" />
-				<input className="form--input" type="text" placeholder="text bottom" />
+				<input
+					className="form--input"
+					type="text"
+					placeholder="text top"
+					name="textOne"
+					value={formData.textOne}
+					onChange={handleChange}
+				/>
+				<input
+					className="form--input"
+					type="text"
+					placeholder="text bottom"
+					name="textTwo"
+					value={formData.textTwo}
+					onChange={handleChange}
+				/>
 				<button onClick={randImg} className="form--btn">
 					Get a new image
 				</button>
-				<img src={memeImg} className="meme-img" />
+				<div className="meme">
+					<img src={formData.randomImage} className="meme-img" />
+					<h2 className="meme-text--top">{formData.textOne}</h2>
+					<h2 className="meme-text--bottom">{formData.textTwo}</h2>
+				</div>
 			</div>
 		</main>
 	);
